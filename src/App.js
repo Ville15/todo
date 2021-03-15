97415e41446dddbd6da2b297c99e030486a9c41f
Ville_L <n0luvi00@students.oamk.fi>
2021-03-15 12:02:54 +0200

    1.st päiv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+
+const URL = "http://localhost/todo/";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  useEffect(() => {
+    let status = 0;
+    fetch(URL + 'retrieve.php')
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json()
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setTasks(res);
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }, [])
+
+  function save(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL + 'add.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: task
+      })
+    })
+    .then(res=> {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setTasks(tasks => [...tasks, res]);
+          setTask('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>Todo List</h3>
+      <div>
+        <form onSubmit={save}>
+          <label>New Task</label>
+          <input value={task} onChange={e => setTask(e.target.value)}/>
+          <button>Save</button>
+        </form>
+      </div>
+      <ol>
+        {tasks.map(task => (
+          <li key={task.id}>{task.description}</li>
+        ))}
+      </ol>
     </div>
-  );
+  )
 }
 
 export default App;
